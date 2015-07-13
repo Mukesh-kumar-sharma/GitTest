27cc9a462c0e6b6cf4d9a57780d1ad75c28fef18
Mukesh Pfizer <mukesh.sharma3@pfizer.com>
2015-07-13 13:46:03 +0530

    Initial commit

---
+++ HelloGit/HelloGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGit
+//
+//  Created by Mukesh  Sharma on 7/13/15.
+//  Copyright (c) 2015 Mukesh  Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloGit/HelloGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloGit
+//
+//  Created by Mukesh  Sharma on 7/13/15.
+//  Copyright (c) 2015 Mukesh  Sharma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
